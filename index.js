e6376800dc559f3061925cda57c38af6b74ec495
Stehan <stehan@fastcomm.com>
2020-11-03 10:37:46 +0200

    code to test if lib works

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-class MiniProgramProxy {}
+class MiniProgramProxy {
+  doSomthing() {
+    window.alert("LIB IS WORKING");
+  }
+}
 
 const miniProgramProxy = new MiniProgramProxy();
 export default miniProgramProxy;
